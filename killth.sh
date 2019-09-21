4bddc4af52b2bdeb8e508b411f61f933ddee5ff5
Leonardo Setti <leonardosetti@gmail.com>
2019-09-21 17:19:45 -0300

    adding changes to killth

--- killth.sh
+++ killth.sh
@@ -2,11 +2,11 @@
 
 ###################################################################################################################################
 # Progarm: killth means Kill That!
-# Version: 0.01
+# Version: 0.02
 # Purpose: This is a simple tool to kill the given program as parameter
 # Author:  Leonardo Setti
 # Created date: 27/11/2018
-# Last modification: 14/03/2019
+# Last modification: 10/09/2019
 # 
 # Change Log:
 #
@@ -37,56 +37,52 @@
 
 clear
 
-echo ""
-echo ""
-echo -n "Enter the program name wanted to kill: "
-echo -n ""
+echo -en Enter the program name wanted to kill:\n
 read TARGET
-echo -n $TARGET "has been chosen as your victim... poor guy!"
-killer(){
 
-for pid  in $CATCHPID; 
+killer(){
+for pid  in ${CATCHPID}; 
 	do kill $pid; 
 	done
-       
-	# kill -- -$CATCHPID
 }
 
-	CATCH_HEAD=`ps -eo ppid,pgid,pid,time,ouid,ruser,pcpu,start,comm | grep PID` 
-	CATCHPID=`ps -eo pid,comm | grep "$TARGET" | awk '{print $1}'`
-	CATCHPPID=`ps -eo ppid,comm | grep "$TARGET" | awk '{print $1}'`
-	CATCHPGID=`ps -eo pgid,comm | grep "$TARGET" | awk '{print $1;exit}'`
-	WHOLESTAT=`ps -eo ppid,pgid,pid,time,ouid,ruser,pcpu,start,comm | grep "$TARGET"` 
-	CAPTURED_TGT=`ps -eo pid,comm | grep "$TARGET" | awk '{print $2;exit}'`
-	PID_LINE=`pidof $CAPTURED_TGT`
-	FLAG=1
+CATCH_HEAD=$(ps -eo ppid,pgid,pid,time,ouid,ruser,pcpu,start,comm|grep -i pid)
+CATCHPID=$(ps -eo pid,comm|grep "${TARGET}"|awk '{print $1}')
+CATCHPPID=$(ps -eo ppid,comm|grep "${TARGET}"|awk '{print $1}')
+CATCHPGID=$(ps -eo pgid,comm|grep "${TARGET}"|awk '{print $1;exit}')
+WHOLESTAT=$(ps -eo ppid,pgid,pid,time,ouid,ruser,pcpu,start,comm|grep "${TARGET}") 
+CAPTURED_TGT=$(ps -eo pid,comm|grep "${TARGET}"|awk '{print $2;exit}')
+PID_LINE=$(pidof ${CAPTURED_TGT})
 	
-	if [ -z "$CATCHPID" ]
+	if [ -z "${CATCHPID}" ]
 		then 	
-			echo "There is no process related to: ""$TARGET"
-			echo""	
+			FLAG=1
+			echo "There is no process related to: "$TARGET"\n"
+			echo -en
 		else
-			echo ""
-		        echo "$CATCH_HEAD"
-			echo "$WHOLESTAT"		
-			echo ""
-			echo "Targeted process refers to ""$CAPTURED_TGT" " PID(s): ""$PID_LINE" "and Process Group:" "$CATCHPGID"
-		        echo ""
-			echo "Killing ""$CAPTURED_TGT"" process PID: ""$PID_LINE"
-			killer
-			echo""
-
-	FLAG=0
+		        echo -en "$CATCH_HEAD"\n
+			echo -en "$WHOLESTAT"\n		
+			echo -en
+			echo -en Targeted process refers to "$CAPTURED_TGT"  PID\(s\): "$PID_LINE" and Process Group: "$CATCHPGID"\n
+			FLAG=0
 	fi
 
 	if [ $FLAG = 0 ] 
 		then 
-			echo "The process has been killed successfully"
+		set -- $(locale LC_MESSAGES)
+		yesptrn="$1"; noptrn="$2"; yesword="$3"; noword="$4"
+		while true; do
+    				read -p "Do you want to kill "$CAPTURED_TGT" - "$PID_LINE" (${yesword} / ${noword})? " yn
+    			case $yn in
+        			${yesptrn##^} ) echo Killing "$CAPTURED_TGT" process PID: "$PID_LINE"\n;killer;;
+        			${noptrn##^} )	echo The process "$PID_LINE" has not been killed and still running.\n; exit;;
+        			* ) echo "Please answer ${yesword} / ${noword}.";;
+    			esac
+		done
+			echo -en The process "$PID_LINE" has been killed successfully!\n
 		else
-			echo "The killing process related to ""$TARGET"" has failed or requested PID doesn't exists"
+			echo -en "The killing process related to " "$TARGET" "has failed or requested PID doesn\'t exists\n" 
 	fi
-
-echo ""
-echo "DONE"
-echo ""
-exit 0
+		echo -en "Bye!\n"
+	exit 0
+kill $!
